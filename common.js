ad88e66db31a0f7460d449980e46691673afebbc
Sergey Vorobyev <syvorobyev@gmail.com>
2025-05-05 15:40:42 +0200

    popups and fixes

--- common.js
+++ common.js
@@ -1,7 +1,15 @@
 import { useScreenTrigger } from "./src/animationTriggers/screen";
 import { useMenu } from "./src/common/menu/script";
 import { addCommonStyles } from "./src/common/styles";
+import { usePopups } from "./src/common/popups/script";
+import { blockScrollOnPopup } from "./src/common/popupBlockScroll/script";
+
+gsap.defaults({
+    duration: 0,
+});
 
 addCommonStyles();
 useScreenTrigger();
 useMenu();
+usePopups();
+blockScrollOnPopup();
